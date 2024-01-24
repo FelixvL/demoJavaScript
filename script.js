957feab23e0574092d548fc1b7fb4602720a4836
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2024-01-24 11:40:09 +0100

    ff

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let counter = 0;
 
+const elMap = {
+    budget : document.querySelector('.budget'),
+}
+
 class BudgetApp{
     get list(){
         console.log("getter");
@@ -9,6 +13,7 @@ class BudgetApp{
         this._list = value;
         console.log("setter",counter++, this.list)
         this.recalculateBudget()
+        this.render()
     }
 
     constructor(){
@@ -24,6 +29,28 @@ class BudgetApp{
         : 0;
 
         console.log(totalIncome)
+        const totalExpenses = this.list.length
+        ? this.list
+            .filter((item)=> item.expense)
+            .reduce((acc, expense)=>(acc += expense.amount),0)
+        : 0;
+
+        console.log(totalIncome)
+
+        this.budgetLeft = totalIncome - totalExpenses
+
+        console.log(this.budgetLeft)
+        const percentageLeft = Math.max((100/totalIncome) * this.budgetLeft,0);
+        const barWith = Math.max((400/100) * percentageLeft,0);
+
+        console.log(percentageLeft);
+        console.log(barWith);
+    }
+    render(){
+        elMap.budget.innerText = new Intl.NumberFormat('nl-NL',{
+            style:'currency',
+            currency:'EUR'
+        }).format(this.budgetLeft);
     }
 
     async fetchTransactions(){
