a9ffdd90ccb3f8e94a29d58c3c0cc29eda81cc58
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2024-01-24 11:11:54 +0100

    eerste berekening

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+let counter = 0;
+
 class BudgetApp{
     get list(){
         console.log("getter");
         return this._list;
     }
     set list(value){
-        console.log("setter")
         this._list = value;
+        console.log("setter",counter++, this.list)
+        this.recalculateBudget()
     }
 
     constructor(){
-        console.log("nieuwe budgetapp");
+        console.log('nieuwe budgetapp');
         this.list = [];
-        console.log("onze lijst", this.list);
+        this.fetchTransactions();
+    }
+    recalculateBudget(){
+        const totalIncome = this.list.length
+        ? this.list
+            .filter((item)=> !item.expense)
+            .reduce((acc, expense)=>(acc += expense.amount),0)
+        : 0;
+
+        console.log(totalIncome)
+    }
+
+    async fetchTransactions(){
+        const response = await fetch(new URL('./transactions.json', import.meta.url))
+        if(response.status === 200){
+            const data = await response.json();
+            this.list = data;
+        }
     }
 
 
